cfc18b5b79265cef798089320bd77a91e5f74dca
AngeloDamante <angelo.damante16@gmail.com>
2021-03-15 03:28:45 +0100

    Optimized Parallel version in CUDA implemented.

---
+++ parallel_CUDA/CUDA/sharedOpt.cu
@@ -0,0 +1,87 @@
+/*
+ * sharedOpt.cu
+ * Main that build parallel version for MM operations with shared memory.
+ *
+ * Images analyzed:
+ *      - bin.png (640x480)
+ *      - mandel.png (1280x960)
+ *      - simpson.png (1920x1200)
+ *      - lion.png (2560x1440)
+ *      - homer.png (3840x2160)
+ *
+ * @Author: AngeloDamante
+ * @mail: angelo.damante16@gmail.com
+ * @GitHub: https://github.com/AngeloDamante
+ *
+ * special thanks to https://github.com/rickie95
+ *
+ */
+
+#include "Image.h"
+#include "Probe.h"
+#include "MM.cuh"
+#include <cstring>
+#include <iostream>
+#include <chrono>
+#include <fstream>
+#include <vector>
+
+const std::string inputPath = "/images/examples/";
+// const std::string outputPath = "/images/gpuResults/";
+const std::string testPath = "/images/tests/";
+
+const std::vector<std::string> operationsName = {"DILATATION", "EROSION",
+                                                 "OPENING", "CLOSING"};
+
+const std::vector<MMop> morphOperations = {DILATATION, EROSION, OPENING,
+                                           CLOSING};
+
+const std::vector<std::string> images = {"bin640x480", "mandel1280x960",
+                                         "simpson1920x1200", "lion2560x1440",
+                                         "homer3840x2160"};
+
+int main(int argc, char const *argv[]) {
+
+    Image *inputImg, *outputImg;
+    Probe *probe = new Square(MASK_RADIUS);
+    std::chrono::high_resolution_clock::time_point start, end;
+    std::chrono::duration<double> span;
+    std::ofstream resultsFile;
+
+    resultsFile.open("/parallelVersion/CUDA/sharedOpt_timing.csv");
+
+    // Every Image
+    for ( const auto &img : images) {
+        inputImg = new Image(inputPath + img + ".png");
+        inputImg->rgb2bw();
+
+        std::cout << "\n" << img << " processing:" << std::endl;
+
+        resultsFile << img << "\n";
+
+        // Every operation in sharedOpt version
+        for (const auto &op : morphOperations) {
+            std::cout << operationsName[op] << "...";
+
+            start = std::chrono::high_resolution_clock::now();
+            outputImg = mm(inputImg, probe, op, SHAREDOPT);
+            end = std::chrono::high_resolution_clock::now();
+            span = end - start;
+            span = std::chrono::duration_cast<std::chrono::duration<double>>(span);
+
+            outputImg->saveImg(testPath + img + "__" + operationsName[op] + ".png");
+            // outputImg->saveImg(outputPath + img + "__" + operationsName[op] + ".png");
+
+            std::cout << span.count() << std::endl;
+            resultsFile << operationsName[op] << ";" << span.count() << "\n";
+        }
+
+        delete inputImg;
+    }
+    delete probe;
+
+    resultsFile.close();
+
+    printf("\n *** Completed! *** \n");
+    return 0;
+}
--- parallel_CUDA/include/MM.cu
+++ parallel_CUDA/include/MM.cu
@@ -197,165 +197,165 @@ void naive::__process(float* imgData, const float*__restrict__ prbData,
     __syncthreads();
 }
 
-// /******* sharedOpt version *******/
-// __global__
-// void sharedOpt::__process(float* imgData, const float* __restrict__ prbData,
-//     float* outData, int imgH, int imgW, int prbH, int prbW, MMop mmOp){
-//
-//     int tx = threadIdx.x;
-//     int ty = threadIdx.y;
-//     int bx = blockIdx.x;
-//     int by = blockIdx.y;
-//     int colImg = bx * blockDim.x + tx;
-//     int rowImg = by * blockDim.y + ty;
-//
-//     // to handle shared memory
-//     extern __shared__ float tileDS[];
-//     int wideTile = TILE_WIDTH + prbW - 1;
-//     int dest = threadIdx.y * wideTile + threadIdx.x;
-//     int destY = dest / wideTile;
-// 	int destX = dest % wideTile;
-//
-//     /// load Tile
-//     if(rowImg >= 0 && rowImg < imgH && colImg >= 0 && colImg < imgW)
-//         tileDS[(destY + prbH / 2) * wideTile + destX + prbW / 2] = imgData[rowImg * imgW + colImg];
-//     else
-//         tileDS[(destY + prbH / 2) * wideTile + destX + prbW / 2] = -1;
-//     __syncthreads();
-//
-//     /// padding
-//     // top row
-//     if(ty == 0){
-//         for(int row = 0; row < prbH/2; row++){
-//             if(rowImg == 0)
-//                 tileDS[row * wideTile + destX + prbW/2] = -1;
-//             else
-//                 tileDS[row * wideTile + destX + prbW/2] = imgData[(rowImg - prbH/2 + row) * imgW + colImg];
-//         }
-//     }
-//
-//     // left column
-//     if(tx == 0){
-//         for(int col = 0; col < prbW/2; col++){
-//             if(colImg == 0)
-//                 tileDS[(destY + prbH/2) * wideTile + col] = -1;
-//             else
-//                 tileDS[(destY + prbH/2) * wideTile + col] = imgData[rowImg * imgW + (colImg - prbW/2 + col)];
-//         }
-//     }
-//
-//     // bottom row
-//     if(ty == TILE_WIDTH - 1){
-//         for(int row = wideTile - prbH/2; row < wideTile; row++){
-//             int gy = rowImg + (row - wideTile + prbH/2);
-//             if(gy >= imgH)
-//                 tileDS[row * wideTile + destX + prbW/2] = -1;
-//             else
-//                 tileDS[row * wideTile + destX + prbW/2] = imgData[gy * imgW + colImg];
-//         }
-//     }
-//
-//     // right column
-//     if(tx == TILE_WIDTH - 1){
-//         for(int col = prbW/2; col < wideTile; col++){
-//             int gx = colImg - wideTile + prbH/2 + col;
-//             if(gx >= imgW)
-//                 tileDS[(destY + prbH/2) * wideTile + col] = -1;
-//             else{
-//                 if(rowImg * imgW + gx > 0)
-//                     tileDS[(destY + prbH/2) * wideTile + col] = imgData[rowImg * imgW + gx];  //FIXME
-//             }
-//         }
-//     }
-//
-//     // NW corner
-//     if(tx == 0 && ty == 0){
-//         for(int row = 0; row < prbH/2; row++){
-//             for(int col = 0; col < prbW/2; col++){
-//
-//                 int gx = colImg - prbW/2 + col;
-//                 int gy = rowImg - prbH/2 + row;
-//
-//                 if(gx < 0 || gy < 0)
-//                     tileDS[row * wideTile + col] = -1;
-//                 else
-//                     tileDS[row * wideTile + col] = imgData[gy * imgW + gx];
-//             }
-//         }
-//     }
-//
-//     // NE corner
-//     if(tx == TILE_WIDTH - 1 && ty == 0){
-//         for(int row = 0; row < prbH/2; row++){
-//             for(int col = TILE_WIDTH + prbH/2; col < wideTile; col ++){
-//                 int gx = colImg + 1 + col;
-//                 int gy = rowImg - prbH/2 + row;
-//
-//                 if(gx >= imgW || gy < 0)
-//                     tileDS[row * wideTile + col] = -1;
-//                 else
-//                     tileDS[row * wideTile + col] = imgData[gy * imgW + gx];
-//             }
-//         }
-//     }
-//
-//     // SW corner
-//     if(tx == 0 && ty == TILE_WIDTH - 1){
-//         for(int row = TILE_WIDTH + prbH/2; row < wideTile; row++){
-//             for(int col = 0; col < prbW/2; col++){
-//                 int gx = colImg - prbW/2 + col;
-//                 int gy = rowImg + 1 + row;
-//
-//                 if(gx < 0 || gy >= imgH)
-//                     tileDS[row * wideTile + col] = -1;
-//                 else
-//                     tileDS[row * wideTile + col] = imgData[gy * imgW + gx];
-//             }
-//         }
-//     }
-//
-//     // SE corner
-//     if(tx == TILE_WIDTH - 1 && ty == TILE_WIDTH -1){
-//         for(int row = TILE_WIDTH + prbH/2; row < wideTile; row++){
-//             for(int col = TILE_WIDTH + prbW/2; col < wideTile; col++){
-//                 int gx = colImg + 1 + col;
-//                 int gy = rowImg + 1 + row;
-//
-//                 if(gx >= imgW || gy >= imgH)
-//                     tileDS[row * wideTile + col] = -1;
-//                 else
-//                     tileDS[row * wideTile + col] = imgData[gy * imgW + gx];
-//             }
-//         }
-//     }
-//
-//     __syncthreads();
-//
-//     /// compute
-//     if(rowImg >= 0 && rowImg < imgH && colImg >= 0 && colImg < imgW){
-//         float max = tileDS[(destY + prbH/2) * wideTile + destX + prbW/2];
-//         float min = max;
-//
-//         for(int rowPrb = - prbH/2; rowPrb < prbH/2; rowPrb++) {
-//             for(int colPrb = - prbW/2; colPrb < prbW/2; colPrb++) {
-//
-//                 // apply mask
-//                 if(tileDS[(destY + prbH/2 + rowPrb) * wideTile + destX + prbW/2 + colPrb] > -1){
-//
-//                     if(max < tileDS[(destY + prbH/2 + rowPrb) * wideTile + destX + prbW/2 + colPrb])
-//                         max = tileDS[(destY + prbH/2 + rowPrb) * wideTile + destX + prbW/2 + colPrb];
-//
-//                     if(min > tileDS[(destY + prbH/2 + rowPrb) * wideTile + destX + prbW/2 + colPrb])
-//                         min = tileDS[(destY + prbH/2 + rowPrb) * wideTile + destX + prbW/2 + colPrb];
-//                 }
-//             }
-//         }
-//
-//         if(mmOp == EROSION)
-//             outData[rowImg * imgW + colImg] = min;
-//
-//         if(mmOp == DILATATION)
-//             outData[rowImg * imgW + colImg] = max;
-//     }
-//     __syncthreads();
-// }
+/******* sharedOpt version *******/
+__global__
+void sharedOpt::__process(float* imgData, const float* __restrict__ prbData,
+    float* outData, int imgH, int imgW, int prbH, int prbW, MMop mmOp){
+
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int colImg = bx * blockDim.x + tx;
+    int rowImg = by * blockDim.y + ty;
+
+    // to handle shared memory
+    extern __shared__ float tileDS[];
+    int wideTile = TILE_WIDTH + prbW - 1;
+    int dest = threadIdx.y * wideTile + threadIdx.x;
+    int destY = dest / wideTile;
+	int destX = dest % wideTile;
+
+    /// load Tile
+    if(rowImg >= 0 && rowImg < imgH && colImg >= 0 && colImg < imgW)
+        tileDS[(destY + prbH / 2) * wideTile + destX + prbW / 2] = imgData[rowImg * imgW + colImg];
+    else
+        tileDS[(destY + prbH / 2) * wideTile + destX + prbW / 2] = -1;
+    __syncthreads();
+
+    /// padding
+    // top row
+    if(ty == 0){
+        for(int row = 0; row < prbH/2; row++){
+            if(rowImg == 0)
+                tileDS[row * wideTile + destX + prbW/2] = -1;
+            else
+                tileDS[row * wideTile + destX + prbW/2] = imgData[(rowImg - prbH/2 + row) * imgW + colImg];
+        }
+    }
+
+    // left column
+    if(tx == 0){
+        for(int col = 0; col < prbW/2; col++){
+            if(colImg == 0)
+                tileDS[(destY + prbH/2) * wideTile + col] = -1;
+            else
+                tileDS[(destY + prbH/2) * wideTile + col] = imgData[rowImg * imgW + (colImg - prbW/2 + col)];
+        }
+    }
+
+    // bottom row
+    if(ty == TILE_WIDTH - 1){
+        for(int row = wideTile - prbH/2; row < wideTile; row++){
+            int gy = rowImg + (row - wideTile + prbH/2);
+            if(gy >= imgH)
+                tileDS[row * wideTile + destX + prbW/2] = -1;
+            else
+                tileDS[row * wideTile + destX + prbW/2] = imgData[gy * imgW + colImg];
+        }
+    }
+
+    // right column
+    if(tx == TILE_WIDTH - 1){
+        for(int col = prbW/2; col < wideTile; col++){
+            int gx = colImg - wideTile + prbH/2 + col;
+            if(gx >= imgW)
+                tileDS[(destY + prbH/2) * wideTile + col] = -1;
+            else{
+                if(rowImg * imgW + gx > 0)
+                    tileDS[(destY + prbH/2) * wideTile + col] = imgData[rowImg * imgW + gx];  //FIXME
+            }
+        }
+    }
+
+    // NW corner
+    if(tx == 0 && ty == 0){
+        for(int row = 0; row < prbH/2; row++){
+            for(int col = 0; col < prbW/2; col++){
+
+                int gx = colImg - prbW/2 + col;
+                int gy = rowImg - prbH/2 + row;
+
+                if(gx < 0 || gy < 0)
+                    tileDS[row * wideTile + col] = -1;
+                else
+                    tileDS[row * wideTile + col] = imgData[gy * imgW + gx];
+            }
+        }
+    }
+
+    // NE corner
+    if(tx == TILE_WIDTH - 1 && ty == 0){
+        for(int row = 0; row < prbH/2; row++){
+            for(int col = TILE_WIDTH + prbH/2; col < wideTile; col ++){
+                int gx = colImg + 1 + col;
+                int gy = rowImg - prbH/2 + row;
+
+                if(gx >= imgW || gy < 0)
+                    tileDS[row * wideTile + col] = -1;
+                else
+                    tileDS[row * wideTile + col] = imgData[gy * imgW + gx];
+            }
+        }
+    }
+
+    // SW corner
+    if(tx == 0 && ty == TILE_WIDTH - 1){
+        for(int row = TILE_WIDTH + prbH/2; row < wideTile; row++){
+            for(int col = 0; col < prbW/2; col++){
+                int gx = colImg - prbW/2 + col;
+                int gy = rowImg + 1 + row;
+
+                if(gx < 0 || gy >= imgH)
+                    tileDS[row * wideTile + col] = -1;
+                else
+                    tileDS[row * wideTile + col] = imgData[gy * imgW + gx];
+            }
+        }
+    }
+
+    // SE corner
+    if(tx == TILE_WIDTH - 1 && ty == TILE_WIDTH -1){
+        for(int row = TILE_WIDTH + prbH/2; row < wideTile; row++){
+            for(int col = TILE_WIDTH + prbW/2; col < wideTile; col++){
+                int gx = colImg + 1 + col;
+                int gy = rowImg + 1 + row;
+
+                if(gx >= imgW || gy >= imgH)
+                    tileDS[row * wideTile + col] = -1;
+                else
+                    tileDS[row * wideTile + col] = imgData[gy * imgW + gx];
+            }
+        }
+    }
+
+    __syncthreads();
+
+    /// compute
+    if(rowImg >= 0 && rowImg < imgH && colImg >= 0 && colImg < imgW){
+        float max = tileDS[(destY + prbH/2) * wideTile + destX + prbW/2];
+        float min = max;
+
+        for(int rowPrb = - prbH/2; rowPrb < prbH/2; rowPrb++) {
+            for(int colPrb = - prbW/2; colPrb < prbW/2; colPrb++) {
+
+                // apply mask
+                if(tileDS[(destY + prbH/2 + rowPrb) * wideTile + destX + prbW/2 + colPrb] > -1){
+
+                    if(max < tileDS[(destY + prbH/2 + rowPrb) * wideTile + destX + prbW/2 + colPrb])
+                        max = tileDS[(destY + prbH/2 + rowPrb) * wideTile + destX + prbW/2 + colPrb];
+
+                    if(min > tileDS[(destY + prbH/2 + rowPrb) * wideTile + destX + prbW/2 + colPrb])
+                        min = tileDS[(destY + prbH/2 + rowPrb) * wideTile + destX + prbW/2 + colPrb];
+                }
+            }
+        }
+
+        if(mmOp == EROSION)
+            outData[rowImg * imgW + colImg] = min;
+
+        if(mmOp == DILATATION)
+            outData[rowImg * imgW + colImg] = max;
+    }
+    __syncthreads();
+}
