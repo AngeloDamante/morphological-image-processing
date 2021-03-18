f966e0494794680f504b4960b77967fd4bbe346f
fabian57fabian <fabian_57@yahoo.it>
2021-03-18 23:46:28 +0100

    Added optimization in mask calculation and variables creation

--- parallel_JAVA/src/parallel_java/MaskApplier.java
+++ parallel_JAVA/src/parallel_java/MaskApplier.java
@@ -20,6 +20,10 @@ public class MaskApplier extends Thread{
     private int mask[];
     private int maskSize;
     private MorphOp operation;//True if Dilation, false if erosion
+    //private int buff[];
+    private int curr_pixel_val;
+    private int image_pixel_val;
+    private int x, y, tx, ty, mr, mc;
     
     public MaskApplier(MyOwnImage img, int output[], int y_min, int y_max, int width, int height, int mask[], int maskSize, MorphOp operation){
         this.img = img;
@@ -32,6 +36,9 @@ public class MaskApplier extends Thread{
         this.mask = mask;
         this.maskSize = maskSize;
         this.operation=operation;
+        //this.buff = new int[9];
+        this.curr_pixel_val = 0;
+        this.image_pixel_val = 0;
     }
     
     @Override
@@ -40,28 +47,36 @@ public class MaskApplier extends Thread{
     }
     
     public void ApplyMask(){
-        for(int y = y_min; y < y_max; y++){
-            for(int x = 0; x < width; x++){
+        for(y = y_min; y < y_max; y++){
+            for(x = 0; x < width; x++){
                 //buff
-                int buff[];
-                buff = new int[9];
-                int i = 0;
-                for(int ty = y - maskSize/2, mr = 0; ty <= y + maskSize/2; ty++, mr++){
-                   for(int tx = x - maskSize/2, mc = 0; tx <= x + maskSize/2; tx++, mc++){
+                //int i = 0;
+                curr_pixel_val = img.getRed(x, y);
+                for(ty = y - maskSize/2, mr = 0; ty <= y + maskSize/2; ty++, mr++){
+                   for(tx = x - maskSize/2, mc = 0; tx <= x + maskSize/2; tx++, mc++){
                        if(ty >= 0 && ty < height && tx >= 0 && tx < width){
                            if(mask[mc+mr*maskSize] != 1){
                                continue;
                            }
                            //pixel under the mask
-                           buff[i] = img.getRed(tx, ty);
-                           i++;
+                           //buff[i] = img.getRed(tx, ty);
+                           //i++;
+                           image_pixel_val = img.getRed(tx, ty);
+                           if(this.operation==MorphOp.Dilation){
+                               if(image_pixel_val > curr_pixel_val)
+                                   curr_pixel_val = image_pixel_val;
+                           }else{
+                               if(image_pixel_val < curr_pixel_val)
+                                   curr_pixel_val = image_pixel_val;
+                           }
                        }
                    }
                 }
 
                 //sort buff
-                java.util.Arrays.sort(buff);
-                output[x+y*width] = this.operation==MorphOp.Dilation ? buff[(maskSize*maskSize) - 1] : buff[(maskSize*maskSize) - i];
+                //java.util.Arrays.sort(buff);
+                //output[x+y*width] = this.operation==MorphOp.Dilation ? buff[(maskSize*maskSize) - 1] : buff[(maskSize*maskSize) - i];
+                output[x+y*width] = curr_pixel_val;
             }
         }
     }
