bb0fd38729f0cce12a1f87813f5c4fc3c55dea7e
fabian57fabian <fabian_57@yahoo.it>
2021-06-20 12:01:08 +0200

    Modified for current datasets

--- create_graphs.py
+++ create_graphs.py
@@ -3,28 +3,27 @@ import csv
 import matplotlib.pyplot as plt
 import pandas as pd
 
-src_folder = 'results'
-out_folder = 'plots/GTX1050TIm'
-#src_folder = 'results/RTX2060'
-#out_folder = 'plots/RTX2060'
+src_folder = 'results_CUDA'
+out_folder = 'plots/CUDA'
 show_plots=False
 
-def create_result_plot(fn:str, transpose=True):
+def create_result_plot(fn:str, transpose=True, debug=False):
     path = os.path.join(src_folder, fn)
     if not os.path.exists(path):
         print("File does not exists: {}".format(fn))
         return
-    print()
+    if debug: print()
     print(fn)
     df = pd.read_csv(path)
     df.set_index("dataset", inplace=True)
-    print(df.head())
+    if debug: print(df.head())
     if transpose: df = df.transpose()
-    print(df.head())
-    print(df.columns)
-    _type, _framework, _version = fn[:-4].split('_')
-    ax = df.plot(title="{} for {} ({} version)".format(_type, _framework, _version), marker='.', markersize=10, figsize=(12, 7))
-    ax.set_xlabel("Tile Width" if _framework == "cuda" else "Threads")
+    if debug: print(df.head())
+    if debug: print(df.columns)
+    _type, _framework, _version, _operation = fn[:-4].split('_')
+    ax = df.plot(title="{} for {} ({} version) on {} operation".format(_type, _framework, _version, _operation), marker='.', markersize=10, figsize=(12, 7))
+    x_label = ("Tile Width" if _framework == "cuda" else "Threads") if not transpose else "dataset size"
+    ax.set_xlabel(x_label)
     ax.set_ylabel("Time (seconds)" if _type == "timings" else "Speedup value")
     if show_plots: plt.show()
     fig = ax.get_figure()
@@ -47,7 +46,9 @@ if __name__ == '__main__':
         os.makedirs(out_folder)
     #create_plots()
     for tr in [True, False]:
-        create_result_plot("speedups_cuda_naive.csv", tr)
-        create_result_plot("speedups_cuda_shared.csv", tr)
+        for file in os.listdir(src_folder):
+            if '_' in file and file.endswith('.csv'):# filter only files created with convert script
+                create_result_plot(file, tr)
+        #create_result_plot("speedups_cuda_shared.csv", tr)
         #àcreate_result_plot("speedups_java_dynamic.csv", tr)
         #create_result_plot("speedups_java_static.csv", tr)
