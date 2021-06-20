55efb79a15e23b13a13353588d5a48f221e93c38
fabian57fabian <fabian_57@yahoo.it>
2021-06-20 12:00:36 +0200

    Ended script convertion to usable csvs

--- results_CUDA/convert_speedups_to_files_for_graphs.py
+++ results_CUDA/convert_speedups_to_files_for_graphs.py
@@ -1,31 +1,46 @@
 import os
 import csv
 
-res_files = "results_CUDA"
-out_folder = "plots/CUDA"
+src_folder = "TMP"
 
 
-def convert_files(fns:list):
-    for fn in fns:
-        datas = {"naive":{}, "optimized":{}}
-        path = os.path.join(res_files, fn)
-        with open(path) as csvfile:
+def convert_files(fns: list):
+    datas = {"naive": {}, "optimized": {}}
+    operations = ["dilatation", "erosion", "opening", "closure"]
+    for k, v in datas.items():
+        for o in operations:
+            v[o] = {}
+    tiles = []
+
+    def reaad_one_tile(fn, datas, tiles):
+        tile_n = int(os.path.basename(fn)[0:2])
+        tiles.append(tile_n)
+        with open(fn) as csvfile:
             reader = csv.reader(csvfile)
             next(reader)  # skip header
             for row in reader:
                 if len(row) == 0: continue
-                # version,resolution,dilatation_mean,dilatation_max,erosion_mean,erosion_max,opening_mean,opening_max,closing_mean,closing_max
-                if row[1] not in datas[row[0]]:
-                    datas[row[0]][row[1]] = []
-                datas[row[0]][row[1]].append(float(row[2]))
+                version, resolution, dilatation_mean, dilatation_max, erosion_mean, erosion_max, opening_mean, opening_max, closing_mean, closing_max = row
+                for i, op in enumerate(operations):
+                    if resolution not in datas[version][op]:
+                        datas[version][op][resolution] = []
+                    datas[version][op][resolution].append(float(row[2 + i * 2].replace(',', '.')))
 
-        with open("speedups_cuda_naive.csv", 'w') as file_naive_out:
-            spamwriter = csv.writer(file_naive_out, delimiter=',')
-            header = ["dataset"] + ["t{}".format(t) for t in tiles]
-            spamwriter.writerow(header)
-            for k in sorted(datas_naive):
-                spamwriter.writerow([k] + datas_naive[k])
+    for f in sorted(fns):
+        reaad_one_tile(f, datas, tiles)
+
+    for ver, d in datas.items():
+        for op, datas_res in d.items():
+            with open("speedups_cuda_{}_{}.csv".format(ver, op), 'w') as file_naive_out:
+                spamwriter = csv.writer(file_naive_out, delimiter=',')
+                header = ["dataset"] + ["t{}".format(t) for t in tiles]
+                spamwriter.writerow(header)
+                for k in sorted(datas_res):
+                    spamwriter.writerow([str(k)] + datas_res[k])
 
 
 if __name__ == '__main__':
-    convert_files(["08TileWidthSpeedup.csv","08TileWidthSpeedup.csv","08TileWidthSpeedup.csv","08TileWidthSpeedup.csv"])
+    files = []
+    for f in os.listdir(src_folder):
+        files.append(os.path.join(src_folder, f))
+    convert_files(files)
